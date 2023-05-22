293bb368bb9e85dcf4856ac2afe3d23c745543c6
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-22 15:45:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+function swapTheme() {
+  var appDiv = document.getElementById('app');
+  var swapButton = document.getElementById('swap');
+
+  if (appDiv.classList.contains('day')) {
+    // Switch to dark theme
+    appDiv.classList.remove('day');
+    appDiv.classList.add('night');
+  } else {
+    // Switch to light theme
+    appDiv.classList.remove('night');
+    appDiv.classList.add('day');
+  }
+}
+
+// Attach the event listener to the button click
+document.getElementById('swap').addEventListener('click', swapTheme);
